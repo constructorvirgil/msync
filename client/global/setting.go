b845971258c637e14dd557ac88047c8c63571496
Kiasma1 <32356017+Kiasma1@users.noreply.github.com>
2020-09-22 17:44:02 +0800

    client框架

---
+++ client/global/setting.go
@@ -0,0 +1,8 @@
+package global
+
+import "client/pkg/setting"
+
+var (
+	ServerSetting *setting.ServerSettingS
+	ClientSetting *setting.ClientSettingS
+)
---
+++ client/go.mod
@@ -0,0 +1,8 @@
+module client
+
+go 1.15
+
+require (
+	github.com/aceld/zinx v0.0.0-20200918102026-219040a04c98
+	github.com/spf13/viper v1.7.1
+)
