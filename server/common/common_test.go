e7501f8058de37d3a758d0d75a2a5812fee9c3cc
constructorvirgil <38716951+constructorvirgil@users.noreply.github.com>
2020-09-22 17:39:06 +0800

    文件封包模块

--- server/common/common_test.go
+++ server/common/common_test.go
@@ -1,6 +1,9 @@
 package common
 
 import (
+	"bytes"
+	"fmt"
+	"math/rand"
 	"testing"
 )
 
@@ -11,19 +14,45 @@ func TestGzipEncode(t *testing.T) {
 	//fmt.Println("src: ", []byte(src))
 	//fmt.Println("zip: ", zip)
 	//fmt.Println("unzip: ", unzip)
-	if !bytesEqual([]byte(src), unzip) {
+	if !bytes.Equal([]byte(src), unzip) {
 		t.Fail()
 	}
 }
 
-func bytesEqual(a []byte, b []byte)bool{
-	if len(a) != len(b) {
-		return false
+func TestFile_Pack(t *testing.T) {
+	file := File{}
+	file.FileId = 0
+	file.FileContent = genRandomBytes(1027)
+
+	bb, err := file.Pack()
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+		return
 	}
-	for i:=0;i<len(a);i++{
-		if a[i] != b[i] {
-			return false
-		}
+
+	nFile := File{}
+	err = nFile.UnPack(bb)
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+		return
 	}
-	return true
+
+	//fmt.Println(file.FileContent)
+	//fmt.Println(nFile.FileContent)
+
+	if !bytes.Equal(file.FileContent, nFile.FileContent) {
+		fmt.Println(err)
+		t.Fail()
+		return
+	}
+}
+
+func genRandomBytes(n int)[]byte{
+	b := []byte{}
+	for i:=0;i<n;i++{
+		b = append(b, byte(rand.Int()))
+	}
+	return b
 }
