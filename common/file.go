bb2aa8fdb7f5c4f28d9491ef60052c017493c196
constructorvirgil <38716951+constructorvirgil@users.noreply.github.com>
2020-09-27 10:08:01 +0800

    传输file结构体新增文件路径信息

--- common/file.go
+++ common/file.go
@@ -3,14 +3,16 @@ package common
 import "encoding/json"
 
 type File struct {
-	FileId []byte
+	FileId      []byte
+	Path        string
 	FileContent []byte
 }
 
 type FilePart struct {
-	FileId       []byte    `json:"fileId"`
+	FileId       []byte `json:"fileId"`
 	FileIndex    int    `json:"fileIndex"`
 	FileMaxIndex int    `json:"fileMaxIndex"`
+	Path         string `json:"path,ommited"`
 	PartContent  []byte `json:"partContent"`
 }
 
@@ -19,33 +21,36 @@ const (
 )
 
 //把一个文件拆分成多个包
-func (c *File) Pack() ([][]byte, error){
+func (c *File) Pack() ([][]byte, error) {
 	bb := [][]byte{}
 
 	if len(c.FileContent) == 0 {
 		return bb, nil
 	}
 
-	partSize := len(c.FileContent)/MaxFilePartSize
-	if (len(c.FileContent)%MaxFilePartSize != 0) {
+	partSize := len(c.FileContent) / MaxFilePartSize
+	if len(c.FileContent)%MaxFilePartSize != 0 {
 		partSize++
 	}
 
 	parts := make([]FilePart, partSize) //分成了partSize个包
 
-	for i:=0;i<len(parts);i++{
+	for i := 0; i < len(parts); i++ {
 		parts[i].FileId = c.FileId
 		parts[i].FileIndex = i
-		parts[i].FileMaxIndex = len(parts)-1
+		parts[i].FileMaxIndex = len(parts) - 1
 
 		b := []byte{}
-		for j:=i*MaxFilePartSize;j<len(c.FileContent) && j<(i+1)*MaxFilePartSize;j++{
+		for j := i * MaxFilePartSize; j < len(c.FileContent) && j < (i+1)*MaxFilePartSize; j++ {
 			b = append(b, c.FileContent[j])
 		}
 		parts[i].PartContent = b
 	}
 
-	for _,p := range(parts){
+	//只有第一个pack存储着文件的路径
+	parts[0].Path = c.Path
+
+	for _, p := range parts {
 		jbyte, err := json.Marshal(p)
 		if err != nil {
 			return bb, err
@@ -57,12 +62,12 @@ func (c *File) Pack() ([][]byte, error){
 }
 
 //从多个包还原回一个文件
-func (c *File) UnPack(bb [][]byte) error{
-	if len(bb) == 0{
+func (c *File) UnPack(bb [][]byte) error {
+	if len(bb) == 0 {
 		return nil
 	}
 	parts := make([]FilePart, len(bb))
-	for i:=0;i<len(bb);i++{
+	for i := 0; i < len(bb); i++ {
 		err := json.Unmarshal(bb[i], &parts[i])
 		if err != nil {
 			return err
@@ -71,7 +76,10 @@ func (c *File) UnPack(bb [][]byte) error{
 
 	c.FileId = parts[0].FileId
 
-	for _,p := range(parts) {
+	//还原文件路径信息
+	c.Path = parts[0].Path
+
+	for _, p := range parts {
 		c.FileContent = append(c.FileContent, p.PartContent...)
 	}
 	return nil
